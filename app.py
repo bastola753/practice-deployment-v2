2f38711a31cfb656e413e7bc971521216658e0d7
bastola753 <bastola753@gmail.com>
2025-12-10 23:54:35 -0600

    api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -16,6 +16,14 @@ def predict():
     prediction = clf.predict(tokenized_email)
     prediction = 1 if prediction[0]==1 else -1
     return render_template("index.html", prediction=prediction, email=email)
+
+@app.route("/api/predict", methods=["POST"])
+def api_predict():
+    email = request.get_json(force=True)
+    tokenized_email = cv.transform([email])
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction==1 else -1
+    return jsonify({prediction: prediction})
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=8080, debug=True)
